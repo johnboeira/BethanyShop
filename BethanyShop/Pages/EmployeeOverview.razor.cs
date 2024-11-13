71c7da7e69b2f8a264b876cf4d8e124ebb617a28
johnboeira <johnboeira@gmail.com>
2024-11-12 21:05:26 -0300

    feat: Adiciona componente para listar funcionários

---
+++ BethanyShop/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,14 @@
+﻿using BethanyShop.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanyShop.Pages;
+
+public partial class EmployeeOverview
+{
+    public List<Employee>? employees { get; set; } = default;
+
+    protected override void OnInitialized()
+    {
+        employees = MockDataService.Employees;
+    }
+}
