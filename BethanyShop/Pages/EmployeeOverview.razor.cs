44ee094d461476b71bac46717e7a2add0f8b9968
johnboeira <johnboeira@gmail.com>
2024-11-15 16:49:13 -0300

    feat : Adiciona Popup e usa EventCallback

--- BethanyShop/Pages/EmployeeOverview.razor.cs
+++ BethanyShop/Pages/EmployeeOverview.razor.cs
@@ -6,9 +6,15 @@ namespace BethanyShop.Pages;
 public partial class EmployeeOverview
 {
     public List<Employee>? Employees { get; set; } = default;
+    private Employee? _selectedEmployee;
 
     protected override void OnInitialized()
     {
         Employees = MockDataService.Employees;
+    }
+
+    public void ShowQuickViewPopup(Employee selectedEmployee)
+    {
+        _selectedEmployee = selectedEmployee;
     }
 }
