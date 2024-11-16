b4e99d02e226b462bcda8ebace50e4071aed2026
johnboeira <johnboeira@gmail.com>
2024-11-16 14:17:27 -0300

    feat : Adiciona tela de detalhes do funcionario, usando href

---
+++ BethanyShop/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,20 @@
+﻿using BethanyShop.Models;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanyShop.Pages;
+
+public partial class EmployeeDetail
+{
+    [Parameter]
+    public string EmployeeId { get; set; }
+
+    public Employee? Employee { get; set; } = new Employee();
+
+    protected override Task OnInitializedAsync()
+    {
+        Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+        return base.OnInitializedAsync();
+    }
+}
