65566941195afc538a4081bf6fd5dd65bf8e1268
johnboeira <johnboeira@gmail.com>
2024-11-18 19:36:56 -0300

    feat : Adiciona foto de perfil usando RenderFragment

---
+++ BethanyShop/Components/ProfilePicture.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanyShop.Components
+{
+    public partial class ProfilePicture
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
