b9bdf156717955111bafc81431d13d428682f01d
johnboeira <johnboeira@gmail.com>
2024-11-30 20:52:00 -0300

    feat : Adiciona tela de editar funcionário usando two way data binding

---
+++ BethanyShop/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,22 @@
+﻿using BethanyShop.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanyShop.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public IEmployeeDataService? EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string? EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+    }
+}
